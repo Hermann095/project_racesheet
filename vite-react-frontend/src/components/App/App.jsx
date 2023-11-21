98923140697b90b124b1607a16a41d359a2c9f08
Hermann095 <tomi_loyer@hotmail.com>
2023-11-21 18:51:39 +0100

    small cleanup

--- vite-react-frontend/src/components/App/App.jsx
+++ vite-react-frontend/src/components/App/App.jsx
@@ -4,7 +4,7 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import HomePage from '../../pages'
 import ResultsPage from '../../pages/results'
 import StandingsPage from '../../pages/standings'
-import Navbar from '../Nabar/Navbar'
+import Navbar from '../Navbar/Navbar'
 import { ThemeProvider } from '../ThemeProvider/ThemeProvider'
 
 import '../../index.css'
